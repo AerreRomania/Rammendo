4f2dc22b82ec32528e79d97569b406700a5b202b
Boris Tosic <boris-ni@hotmail.rs>
2021-02-14 17:02:31 +0100

    identity and datatypes bugfixes

--- src/CsvImporter/CsvInfo.cs
+++ src/CsvImporter/CsvInfo.cs
@@ -7,30 +7,38 @@ namespace CsvImporter
     {
         public const string CSV_PATH = "D:\\Rammendo Import";
         public const string CONNECTION_STRING = "data source=192.168.96.37;initial catalog=ONLYOU; User ID=nicu; password=onlyouolimpias;";
-        
-        public static int GetMaxKey(string fileName) {
+
+        private static readonly Log _log = new Log();
+
+        public static int GetMaxKeyBasedOnImport(string fileName) {
 
             var qry = @"
 SELECT MAX(LastKey) 
-FROM RammendoImport
+FROM RammendoImport 
 WHERE FileKey=@fileName;";
-            var maxKey = 0;
 
-            using (var conn = new SqlConnection(CONNECTION_STRING)) {
-                var cmd = new SqlCommand(qry, conn);
-                cmd.Parameters.Add("@fileName", SqlDbType.NVarChar).Value = fileName;
-                conn.Open();
-                var dr = cmd.ExecuteReader();
-                if (dr.HasRows) {
-                    while (dr.Read()) {
-                        int.TryParse(dr[0].ToString(), out maxKey);
+            try {
+                var maxKey = 0;
+
+                using (var conn = new SqlConnection(CONNECTION_STRING)) {
+                    var cmd = new SqlCommand(qry, conn);
+                    cmd.Parameters.Add("@fileName", SqlDbType.NVarChar).Value = fileName;
+                    conn.Open();
+                    var dr = cmd.ExecuteReader();
+                    if (dr.HasRows) {
+                        while (dr.Read()) {
+                            int.TryParse(dr[0].ToString(), out maxKey);
+                        }
                     }
+                    conn.Close();
+                    dr.Close();
                 }
-                conn.Close();
-                dr.Close();
+                return maxKey;
             }
-
-            return maxKey;
+            catch (System.Exception ex) {
+                _log.WriteLog($"[GETMAXKEY] Error: {ex.Message}");
+                return 0;
+            }
         }
     }
 }
--- src/CsvImporter/Importer.cs
+++ src/CsvImporter/Importer.cs
@@ -7,84 +7,79 @@ namespace CsvImporter
 {
     public class Importer
     {
-        private readonly Log log = new Log();
+        private readonly Log _log = new Log();
         private DataTable _dataTable = new DataTable();
 
         public void Import() {
             try {
-                var executionTime = DateTime.Now;
+                var startImportTime = DateTime.Now;
                 var strNameByDate = DateTime.Now.ToString("dd-MM-yyyy");
 
                 foreach (var file in Directory.GetFiles(CsvInfo.CSV_PATH)) {
                     var fileName = Path.GetFileNameWithoutExtension(file);
                     if (fileName != strNameByDate) continue;
 
-                    log.WriteLog("[ACCESS] Trying to access and read " + fileName + ".csv\n" + Path.GetFullPath(file));
+                    _log.WriteLog("[ACCESS] Trying to access and read " + fileName + ".csv " + Path.GetFullPath(file));
 
                     _dataTable = new DataTable();
                     CreateDedicatedColumns(_dataTable);
 
-                    var maxKey = CsvInfo.GetMaxKey(fileName);
+                    var maxKey = CsvInfo.GetMaxKeyBasedOnImport(fileName);
 
                     using (StreamReader streamReader = new StreamReader(new FileStream(file, FileMode.Open, FileAccess.Read))) { 
                         while (!streamReader.EndOfStream) {
                             string[] rows = streamReader.ReadLine().Split(';');
-                            int.TryParse(rows[0].ToString(), out var maxIdentity);
-                           
-                            if (maxIdentity > maxKey) {
-                                var newRow = _dataTable.NewRow();
-                                for (var i = 1; i <= rows.Length - 1; i++) { newRow[i - 1] = rows[i]; }
-                                newRow[10] = maxIdentity;
-                                newRow[11] = fileName;
-                                _dataTable.Rows.Add(newRow);
-                            }
+                            int.TryParse(rows[0].ToString(), out var maxIdentityKey);
+                            if (maxIdentityKey <= maxKey) continue; 
+
+                            var newRow = _dataTable.NewRow();
+                            newRow[0] = 0;
+                            for (var i = 1; i <= rows.Length - 1; i++) { newRow[i] = rows[i]; }
+                            newRow[11] = maxIdentityKey;
+                            newRow[12] = fileName;
+                            _dataTable.Rows.Add(newRow);
                         }
                     }
                 }
 
                 if (_dataTable.Rows.Count == 0) {
-                    log.WriteLog(message: "[EMPTY] File " + strNameByDate + ".csv has been opened correctly but no new records were loaded.");
+                    _log.WriteLog(message: "[EMPTY] File " + strNameByDate + ".csv has been opened correctly but no new records were loaded.");
                     return;
                 }
-
-                if (_dataTable.Rows.Count != 0) {
-                    var connection = new SqlConnection(CsvInfo.CONNECTION_STRING);
-
-                    using (var sbc = new SqlBulkCopy(connection)) {
-                        sbc.DestinationTableName = "RammendoImport";
-                        connection.Open();
-                        sbc.WriteToServer(_dataTable, DataRowState.Added);
-                        connection.Close();
-                        sbc.Close();
-                    }
-
-                    var ms = DateTime.Now.Subtract(executionTime).TotalMilliseconds;
-                    log.WriteLog(message:
-                          $"CSV FILE {strNameByDate}.csv WITH {_dataTable.Rows.Count} rows loaded " +
-                          $"with status [OK]; " +
-                          $"Estimated time: {ms}ms");
+                
+                using (var sbc = new SqlBulkCopy(CsvInfo.CONNECTION_STRING, SqlBulkCopyOptions.Default)) {
+                    sbc.DestinationTableName = "RammendoImport";
+                    sbc.WriteToServer(_dataTable, DataRowState.Added);
                 }
+
+                var ms = DateTime.Now.Subtract(startImportTime).TotalMilliseconds;
+                _log.WriteLog(message:
+                      $"CSV FILE {strNameByDate}.csv WITH {_dataTable.Rows.Count} rows loaded " +
+                      $"with status [OK]; " +
+                      $"Estimated time: {ms}ms");
+
             }
             catch (UnauthorizedAccessException uex) {
-                log.WriteLog(message: "! Deny " + uex.Message);
+                _log.WriteLog(message: "! Deny " + uex.Message);
             }
             catch (Exception ex) {
-                log.WriteLog(message: "! Deny " + ex.Message);
+                _log.WriteLog(message: "! Deny " + ex.Message);
             }
         }
-
+        /*14/02/2021 16:29:17: ! Deny Input string was not in a correct format.Couldn't store <14-02-2021> in LastKey Column.  Expected type is Int32.*/
         private void CreateDedicatedColumns(DataTable dataTable) {
+            dataTable.Columns.Add("Id", typeof(int));
             dataTable.Columns.Add("Commessa");
             dataTable.Columns.Add("Article");
             dataTable.Columns.Add("Color");
             dataTable.Columns.Add("Gradient");
             dataTable.Columns.Add("Size");
             dataTable.Columns.Add("Component");
-            dataTable.Columns.Add("QtyPack");
+            dataTable.Columns.Add("QtyPack", typeof(int));
             dataTable.Columns.Add("Barcode");
-            dataTable.Columns.Add("Good");
-            dataTable.Columns.Add("Bad");
-            dataTable.Columns.Add("LastKey");
+            dataTable.Columns.Add("Good", typeof(int));
+            dataTable.Columns.Add("Bad", typeof(int));
+            dataTable.Columns.Add("LastKey", typeof(int));
             dataTable.Columns.Add("FileKey");
         }
     }
