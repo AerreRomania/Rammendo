58502f127722756eaab466c3a5a9b2ed1f44b97d
boristosic <32439569+boristosic@users.noreply.github.com>
2021-02-27 18:34:08 +0100

    Delete src/Rammendo.Dom directory

--- src/Rammendo.Dom/Repositories/BaseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rammendo.Dom.Repositories
-{
-    public class BaseRepository
-    {
-        protected string ConnectionString {
-            get => "data source=192.168.96.37;initial catalog=ONLYOU; User ID=nicu; password=onlyouolimpias;";
-        }
-    }
-}
